bf10d0fe6185237cf278e8eaa6d46ea6bd276c41
Saqib Younas <muhammadsaqibyounas11@gmail.com>
2026-03-31 04:19:55 +0500

    Update the issues

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -9,7 +9,7 @@ export default function Login() {
       {' '}
       <header className="absolute top-0 left-0 w-full z-20">
         <div className="flex items-center p-4 md:p-6">
-          <Link href="/login">
+          <Link href="/">
             <Image
               className="rounded-4xl"
               src="/Logo.jpg"
